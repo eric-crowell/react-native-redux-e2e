b270218169acdcd8fb40b4b9d1f44be22bbd312f
Eric Crowell <eric.crowell@ecrow.dev>
2023-03-03 16:26:06 +0100

    dev: Added apk release files

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import { createEntityAdapter } from '@reduxjs/toolkit';
+import type { Post } from '../src/app/services/post';
 
 const app = express();
 const port = 3000;
+
+const adapter = createEntityAdapter<Post>()
+let state;
+
+export const getMockState = () => {
+  if (!state) {
+    state = adapter.getInitialState();
+    state = adapter.setAll(state, [
+      { id: 1, name: 'A sample post', fetched_at: new Date().toUTCString() },
+      {
+        id: 2,
+        name: 'A post about rtk-query',
+        fetched_at: new Date().toUTCString(),
+      },
+    ]);
+  }
+  return state;
+}
 
 app.get('/time/:offset', async (req, res) => {
     console.log('Received request for time');
@@ -16,6 +36,15 @@ app.get('/time/:offset', async (req, res) => {
     await new Promise((resolve) => setTimeout(resolve, 400));
 
     res.status(200).json({ time: new Date(target).toUTCString() });
+});
+
+app.get('/posts/:id', async (req, res) => {
+    const { id } = req.params as { id: string }
+    state = adapter.updateOne(state, {
+      id,
+      changes: { fetched_at: new Date().toUTCString() },
+    })
+    res.status(200).json(state.entities[id]);
 });
 
 function start() {
